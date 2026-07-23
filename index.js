d3607951791bd745f79fb8957858d769f401f570
Duke Okarigit config --global use.email omwagaduke@gmail.com <omwagaduke@gmail.com>
2026-07-23 14:56:47 +0300

    fixed photo gallery

---
+++ index.js
@@ -0,0 +1,98 @@
+console.log("John Doe")
+
+console.log(100<300)
+
+console.log(["John Doe", 123, 345, true,[12,2,3,4,]])
+
+console.log({
+    name: "John Doe",
+    age: 22,
+    hobbies: ["Swimming", "singing"]
+})
+
+let person = "John Doe"
+//access
+console.log(person)
+
+//reuse
+console.log(person)
+console.log(person)
+console.log(person)
+
+//use
+console.log("My name is "+ person)
+
+let age = 56
+let gender = "female"
+
+if(age > 18 && gender == "female"){
+    console.log("you are welcome")
+}else if(age < 45){
+    console.log("sorry! you are too old")
+}
+
+
+
+
+
+
+
+
+
+
+let nums = [85, 92, 78, 90];
+console.log(nums[0])
+
+for(let i = 0; i<nums.length; i++){
+    console.log("Hello world!", i)
+}
+
+let i = 0;
+while(i < nums.length){
+    console.log("Testing While loop", i)
+    i++
+}
+console.log(2 + 5)
+
+console.log(7 + 5)
+
+
+function addNum(num1, num2){
+    // func body
+    let num = 1
+    return
+}
+
+console.log(addNums(100, 1))
+
+function change(num){
+    let test = "Testing"
+    if(num === test){
+let number = 23
+        return true
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
